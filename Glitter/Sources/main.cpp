42cc592434a1401cf74cff381ae5f2ca27dd4ee0
Nick Desaulniers <nick.desaulniers@gmail.com>
2017-02-17 22:13:55 -0800

    create Shape class
    Build the triangle from an instance of Shape

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -9,8 +9,45 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
-void setup() {
+class Shape {
+public:
+  std::vector<glm::vec2> m_vertices;
+  GLuint m_vao;
+  GLuint m_vbo;
+  GLint m_posAttrib;
+  Shape(std::vector<glm::vec2> vertices, GLint posAttrib): m_vertices(std::move(vertices)),
+      m_posAttrib(posAttrib) {
+    glGenVertexArrays(1, &m_vao);
+    glBindVertexArray(m_vao);
+
+    glGenBuffers(1, &m_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+    glBufferData(GL_ARRAY_BUFFER, m_vertices.size() * sizeof(glm::vec2), &m_vertices[0], GL_STATIC_DRAW);
+
+    glVertexAttribPointer(m_posAttrib, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glEnableVertexAttribArray(m_posAttrib);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    print_vertices();
+  }
+  void draw() const {
+    glBindVertexArray(m_vao);
+    glDrawArrays(GL_TRIANGLES, 0, m_vertices.size());
+    glBindVertexArray(0);
+  }
+  void print_vertices() const {
+    std::cout << "created polygon with " << m_vertices.size() << " vertices {" << std::endl;
+    for (const glm::vec2& vertex_pair : m_vertices) {
+      std::cout << "  " << vertex_pair[0] << ", " << vertex_pair[1] << std::endl;
+    }
+    std::cout << "}" << std::endl;
+  }
+};
+
+void setup(std::vector<Shape>& shapes) {
   const GLchar* vshader_src =
     "#version 150\n"
     "in vec2 position;"
@@ -65,24 +102,15 @@ void setup() {
     std::cout << message << std::endl;
   }
   glUseProgram(program);
-
-  GLuint vao;
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
-
-  float vertices[] = {
-     0.0f,  0.5f,
-     0.5f, -0.5f,
-    -0.5f, -0.5f
-  };
-  GLuint vbo;
-  glGenBuffers(1, &vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
   GLint posAttrib = glGetAttribLocation(program, "position");
-  glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
-  glEnableVertexAttribArray(posAttrib);
+
+  std::vector<glm::vec2> vertices = {
+    {  0.0f,  0.5f },
+    {  0.5f, -0.5f },
+    { -0.5f, -0.5f }
+  };
+  Shape triangle(vertices, posAttrib);
+  shapes.push_back(triangle);
 }
 
 int main(int argc, char * argv[]) {
@@ -107,7 +135,8 @@ int main(int argc, char * argv[]) {
   gladLoadGL();
   fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
-  setup();
+  std::vector<Shape> shapes;
+  setup(shapes);
 
   // Rendering Loop
   while (glfwWindowShouldClose(mWindow) == false) {
@@ -118,7 +147,9 @@ int main(int argc, char * argv[]) {
     glClearColor(0.25f, 0.5f, 0.25f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    for (const Shape& shape: shapes) {
+      shape.draw();
+    }
 
     // Flip Buffers and Draw
     glfwSwapBuffers(mWindow);
